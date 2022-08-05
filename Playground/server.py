8df1658b874885f920ad1ae75bf5de47386eef10
Evelyn Valles <106193803+evelynvalles@users.noreply.github.com>
2022-08-04 20:57:41 -0500

    first commit

---
+++ Playground/server.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/play')
+def index():
+    return render_template("index.html")
+
+@app.route('/play/<int:num>')
+def box_mult(num):
+
+    return render_template("index2.html", num=num)
+
+@app.route('/play/<int:num>/<string:color>')
+def box_color(num, color):
+
+    return render_template("index3.html", num=num, color=color)
+
+if __name__=="__main__": 
+    app.run(debug=True)  
